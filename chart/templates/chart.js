e83044f72db994ab47cf20bf3437e44a679120b1
Bethwel Kiplimo <bkiplimo@princeton.edu>
2022-03-07 14:58:22 -0500

    use plotly instead of chart.js

---
+++ chart/templates/chart.js
@@ -0,0 +1,103 @@
+const chrt = document.getElementById('line')
+const graph = new Chart(chrt, {
+    type: 'line',
+    data: {
+        labels: label, // {{ label| safe }},
+        datasets: [{
+            label: 'Temperature (degC)',
+            data: temp, // {{ temp }},
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: 'Humidity Levels (%)',
+            data: {{ hum }},
+    backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+    ],
+    borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ],
+    borderWidth: 1
+},
+
+    {
+        label: 'CO2 Concentration (ppm)',
+        data: gas, //{{ gas }},
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1,
+    }
+    ]
+        },
+options: {
+    scales: {
+        y: {
+            beginAtZero: true
+        },
+        xAxes: {
+            type: 'time',
+                // time: {
+                //     // Luxon format string
+                //     tooltipFormat: 'DD T'
+                // },
+                title: {
+                display: true,
+                    text: 'Date'
+            }
+        }
+    },
+    plugins: {
+        title: {
+            display: true,
+                text: 'Chart Title',
+      }
+    }
+
+}
+   
+
+
+
+
+
+    });
--- chart/views.py
+++ chart/views.py
@@ -7,6 +7,10 @@ from .models import Measurements #, Temp, CO2, Hum
 from django.http import JsonResponse
 import datetime
 import csv
+import plotly.express as px
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+import pandas as pd
 
 # def initialize_db():
     
@@ -30,11 +34,36 @@ import csv
 #             if len(hum) > 0:
 #                 h = Hum( hum=hum)
 #                 h.save()
+def generate_csv():
+    file = '/Users/bethwelkiplimo/desktop/mae345_2022/feeds.csv'
+    print(file)
+    data = Measurements.objects.all()
+
+    with open(file,'w') as f:
+        options = ["date",'time', 'co2','temp', 'hum']
+        for i in range(5):
+            f.write(options[i])
+            if i != 4:
+                f.write('\t')
+        f.write('\n')
+        for r in data:
+            f.write(str(r.date))
+            f.write('\t')
+            f.write(str(r.time))
+            f.write('\t')
+            f.write(str(r.co2))
+            f.write('\t')
+            f.write(str(r.temp))
+            f.write('\t')
+            f.write(str(r.hum))
+            f.write('\n')
+
+
 
 
 
 def temperature(request, today = False, yesterday = False, two_hour = False):
-
+    # generate_csv()
     if today:
         raw_data = Measurements.get_today()
     elif two_hour:
@@ -46,14 +75,36 @@ def temperature(request, today = False, yesterday = False, two_hour = False):
 
     label, gas, temp, hum = [],[],[],[]
     print(len(raw_data))
+    j = 0
     for i, dat in enumerate(raw_data[:min(len(raw_data), 100)]):
-        lab = dat.date.strftime("%m/%-d/%Y, %H:%M:%S")
+        lab = dat.date + datetime.timedelta(days=j)
+        # lab = lab.strftime("%m/%-d/%Y, %H:%M:%S")
         hum.append(float(dat.hum))
         gas.append(float(dat.co2))
-        label.append(""+dat.date.strftime("%m/%-d/%Y,")+""+dat.time.strftime("%H:%M:%S"))
+        label.append(lab)#""+str(j)+dat.date.strftime("%m/%-d/%Y,")+""+dat.time.strftime("%H:%M:%S"))
         temp.append(float(dat.temp))
+        j += 1
+    df = pd.DataFrame(gas, index = label)
+    h = pd.Series(hum)
+    df += pd.Series(hum)
+    df['temp'] = pd.Series(temp)
+    print(df)
+    graph_2 = plot([Scatter(x = label, y = hum,
+                        mode='lines', name='humidity',
+                        opacity=0.8, marker_color='blue'), Scatter(x = label, y = temp,
+                        mode='lines', name='temperature',
+                        opacity=0.8, marker_color='red')],
+               output_type='div')
+
+    graph_co = plot([Scatter(x = label, y = gas,
+                        mode='lines', name='Carbon(IV)Oxide (CO_2)',
+                        opacity=0.8, marker_color='green', showlegend = True)],output_type='div')
+
+    
+    graph = px.line(x  = label, y = hum)
+    print(graph)
     print(len(gas), len(label))
-    return render(request, 'graph.html', {'label':label,'gas':gas, 'temp':temp, 'hum':hum })
+    return render(request, 'graph.html', {'graph_co': graph_co, 'graph': graph_2, 'label':label,'gas':gas, 'temp':temp, 'hum':hum })
 
 def update(request, co,temp,hum):
     now = datetime.datetime.now().strftime("%m/%-d/%Y, %H:%M:%S")
